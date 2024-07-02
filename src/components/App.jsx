a708b4ef661d2eca91cc131cc177b2ad779f7fad
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-07-02 17:37:16 +0200

    Several states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,31 @@
-/* export const App = () => {
-	let clicks = 0;
-
-  const handleClick = () => {
-    clicks = clicks + 1;
-  };
-
-	return <button onClick={handleClick}>Current: {clicks}</button>
-}; */
-
-/* Обробник подій handleClick оновлює локальну змінну clicks. Проте дві речі заважають змінам бути видимими:
-- Локальні змінні не зберігаються між рендерами. Коли React рендерить цей компонент вдруге, він рендерить його з нуля — не враховує жодних змін у локальних змінних.
-- Зміни локальних змінних не викликають повторних рендерів. React не усвідомлює необхідності повторного рендерингу компонента з новими даними. */
-
-
-/* Для оновлення компонента новими даними потрібно виконати дві речі:
-
-Зберегти дані між рендерами.
-Заставити React рендерити компонент з новими даними (повторний рендеринг).
- */
-
-/* Тут синтаксис [ clicks, setClicks ] - це деструктуризація масиву. Масив, який повертає useState, завжди має точно два елементи: перший (clicks) - це змінна стану, а другий (setClicks) - це функція для її встановлення.
-
-У handleClick використовуємо setClicks для зміни стану clicks. */
-
 import { useState } from "react";
 
+/* Декілька станів
+Ви можете мати стільки змінних стану, скільки потрібно в одному компонентії. Для кожного використовуйте окремий useState.
+ */
+
+/* Додамо до компонента App ще один стан isOpen, який буде контролювати відображення абзацу тексту. */
+
 export const App = () => {
- const [ clicks , setClicks] = useState(0); /* Це деструктуризація масиву і повертається масив */
+  const [clicks, setClicks] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setClicks(clicks + 1);
-  }
+  };
 
-  return <button onClick={handleClick}>Current: {clicks}</button>
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button onClick={handleClick}>Current: {clicks}</button>
+      <button onClick={handleToggle}>{isOpen ? "Hide" : "Show"}</button>
+      {isOpen && <p>Now you can see me!</p>}
+    </>
+  );
 };
+
+/* Кожен зі станів clicks та isOpen незалежний і оновлюється своїми відповідними функціями встановлення setClicks та setIsOpen відповідно.
+ */
