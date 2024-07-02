9171819f81a036796883a811a58922f139d7e785
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-07-02 17:47:12 +0200

    Isolation of the state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,15 @@
-import { useState } from "react";
-
-/* Декілька станів
-Ви можете мати стільки змінних стану, скільки потрібно в одному компонентії. Для кожного використовуйте окремий useState.
- */
-
-/* Додамо до компонента App ще один стан isOpen, який буде контролювати відображення абзацу тексту. */
+import ClickCounter from "./ClickCounter/ClickCounter"
 
 export const App = () => {
-  const [clicks, setClicks] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleClick = () => {
-    setClicks(clicks + 1);
-  };
-
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <>
-      <button onClick={handleClick}>Current: {clicks}</button>
-      <button onClick={handleToggle}>{isOpen ? "Hide" : "Show"}</button>
-      {isOpen && <p>Now you can see me!</p>}
+    <ClickCounter/>
+    <ClickCounter/>
     </>
-  );
-};
+  )
+}
 
-/* Кожен зі станів clicks та isOpen незалежний і оновлюється своїми відповідними функціями встановлення setClicks та setIsOpen відповідно.
+export default App
+/* 
+Зверніть увагу на те, як компонент App нічого не знає про стан ClickCounter. На відміну від props, стан є повністю приватним для компонента, що його визначає. Батьківський компонент не може його змінити. Це дозволяє додавати стан до будь-якого компонента чи видаляти його без впливу на решту компонентів.
  */
