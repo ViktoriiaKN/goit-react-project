edeaaab56ff567c45aa501d8d561b38950acd7e4
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-07-08 17:17:29 +0200

    form again

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
-import { useEffect, useState} from 'react';
+
 import LoginForm from './LoginForm/LoginForm';
 
 export const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    console.log("Effect");
-
-    return () => {
-      console.log("Clean up");
-    };
-  });
 
   return (
     <div>
