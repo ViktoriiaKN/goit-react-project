4f37ebde51d2fa0ca57c4a115988b7b9549d6c62
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-07-09 18:10:06 +0200

    Controlled elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,12 @@
 
-import LoginForm from './LoginForm/LoginForm';
-import MyComponent from './MyComponent/MyComponent';
+import SearchBar from './SearchBar/SearchBar';
 
 export const App = () => {
-const handleLogin = (userData) => {
-  console.log(userData);
-};
 
   return (
     <div>
     <h1>Please login to your account!</h1>
-      {/* Передаємо колбек як пропс форми */}
-     <LoginForm onLogin={handleLogin}/>
-     <MyComponent/>
+     <SearchBar/>
     </div>
   );
 };
