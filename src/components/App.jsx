b199a1c8791fa5758a56f8ca610a6fd883cd01c2
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-07-16 17:23:52 +0200

    SearchForm and App whithout useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,32 +2,34 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import ArticleList from './ArticleList/ArticleList';
 import { fetchArticlesWithTopic } from '../article-api';
+import SearchForm from './SearchForm/SearchForm';
 
 export const App = () => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  useEffect(() => {
-    async function fetchArticles() {
-      try {
-        setLoading(true); 
-        // 2. Використовуємо HTTP-функцію
-        const data = await fetchArticlesWithTopic('react');
-        setArticles(
-         data
-        );
-      } catch(error) {
-setError(true);
-      } finally {
-        setLoading(false)
-      }
-    }
-    fetchArticles();
-  }, []);
+  /* Оскільки тепер користувач сам вводить рядок для пошуку статей, нам не потрібний ефект. Таким чином, будемо писати код всередині функції handleSearch, яка виконується при сабміті форми. Робимо її асинхронною і додаємо всередину код, пов'язаний з HTTP-запитом.
+
+ */
+
+  const handleSearch = async(topic) => {
+try {
+  setArticles([]);
+  setError(false);
+  setLoading(true);
+  const data = await fetchArticlesWithTopic(topic);
+  setArticles(data);
+} catch (error) {
+  setError(true);
+} finally {
+  setLoading(false)
+}
+  }
 
   return (
     <div>
+      <SearchForm onSearch={handleSearch}/>
       <h1>Latest articles</h1>
       {loading && <p>Loading data, please wait</p>}
       {error && (<p>Whoops, something went wrong! Please try reloading this page!</p>)}
