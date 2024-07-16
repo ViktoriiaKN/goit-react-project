8e129604d3b5276f86f461c68311507b60d0867f
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-07-16 16:31:27 +0200

    error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import ArticleList from './ArticleList/ArticleList';
 export const App = () => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     async function fetchArticles() {
@@ -18,7 +19,7 @@ export const App = () => {
           response.data.hits
         );
       } catch(error) {
-
+setError(true);
       } finally {
         // 2. Встановлюємо індикатор в false після запиту
         setLoading(false)
@@ -31,6 +32,7 @@ export const App = () => {
     <div>
       <h1>Latest articles</h1>
       {loading && <p>Loading data, please wait</p>}
+      {error && (<p>Whoops, something went wrong! Please try reloading this page!</p>)}
       {articles.length > 0 && <ArticleList articles={articles} />}
     </div>
   );
